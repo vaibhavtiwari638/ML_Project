195022db2df10892bea48bc90bf2076b17040aac
vaibhavtiwari638 <156024215+vaibhavtiwari638@users.noreply.github.com>
2024-01-10 02:21:09 +0530

    Add files via upload

---
+++ Myapp/urls.py
@@ -0,0 +1,9 @@
+from tokenize import Name
+from unicodedata import name
+from django.urls import path
+from Myapp.views import *
+urlpatterns=[
+    path('',login,name='login'),
+    path('signin',fun1,name="login"),
+    path('signup',fun2,name="login"),
+]
---
+++ Myapp/views.py
@@ -0,0 +1,26 @@
+import sys
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+import datetime
+
+
+def login(request):
+    return render( request ,'loginpage.html');
+def fun1(request):
+      email=request.POST['email']
+      password=request.POST['password']
+      try :
+            return HttpResponse('<b>Email :- </b>'+email+'<br> <b>Password :- </b>' + password +' <br> ')
+      except:
+           return HttpResponse(email)
+def fun2(request):
+      first_name=request.POST['first_name']
+      last_name=request.POST['last_name']
+      username=request.POST['username']
+      email=request.POST['email']
+      password=request.POST['password']
+      address_line1=request.POST['address_line1']
+      city=request.POST['city']
+      state=request.POST['state']
+      pincode=request.POST['pincode']
+      return HttpResponse('<b>First Name :- </b>'+first_name+'<br> <b>last_name:- </b>' +last_name +' <br> <b>Email :- </b>'+email+'<br> <b>Password :- </b>' + password +' <br> <b>username :- </b>'+username+'<br> <b>Address :- </b>' + address_line1+',' +city+',' +state+','+pincode+' <br> ')
